122f8cbbbcb7c42f7debdaec180df595a82c149f
shinseunghwan <ssh2375@naver.com>
2023-12-10 14:36:57 +0900

    중간진척보고#02

--- Sources/main-HangManproject.c
+++ Sources/main-HangManproject.c
@@ -26,8 +26,8 @@ char* getRandomWord() {
 
 void playHangman() {
     //게임에 필요한 변수 선언 및 초기화
-    char* wordGuess = getRandomWord();
-    int wordLength = strlen(wordGuess);
+    char* wordToGuess = getRandomWord();
+    int wordLength = strlen(wordToGuess);
     char guessedWord[MAX_WORD_LENGTH];
     int numTries = 0;
 
@@ -38,7 +38,7 @@ void playHangman() {
     //선택된 단어의 절반은 visibleWord 배열에 복사하고 나머지는 플레이어에게 보여주지않도록한다.
     for (int i = 0; i < wordLength; i++) {
         if (i < numVisibleChars) {
-            visibleWord[i] = wordGuess[i];
+            visibleWord[i] = wordToGuess[i];
         }
         else {
             visibleWord[i] = '_';
@@ -53,5 +53,39 @@ void playHangman() {
 
     printf("행맨 게임을 시작합니다. 단어를 맞춰보세요!\n");
 
+    //단어 상태를 플레이어에게 보여줌
+    while (numTries < MAX_TRIES && numCorrectGuesses < wordLength) {
+        printf("단어: %s\n", visibleWord);
+        printf("추측한 단어를 입력하세요: ");
+
+        //플레이어에게 한 글자 추측받음
+        char guess;
+        scanf(" %c", &guess);
+
+        //추측한 글자가 단어에 있는지 반복문과 조건문을 통해 확인
+        int found = 0;
+        for (int i = 0; i < wordLength; i++) {
+            if (wordToGuess[i] == guess && visibleWord[i] == '_') {
+                visibleWord[i] = guess;
+                found = 1;
+                numCorrectGuesses++;
+            }
+        }
+        //추측 결과에 따라서 플레이어에게 힌트 제공
+        if (!found) {
+            numTries++;
+            printf("틀렸습니다. 남은 시도 횟수: %d\n", MAX_TRIES - numTries);
+        }
+        else {
+            printf("맞췄습니다!\n");
+        }
+    }
+    //게임 종료 메시지 출력
+    if (numCorrectGuesses == wordLength) {
+        printf("축하합니다! 정답은 \"%s\" 였습니다!\n", wordToGuess);
+    }
+    else {
+        printf("아쉽습니다. 정답은 \"%s\" 였습니다.\n", wordToGuess);
+    }
 
 }
